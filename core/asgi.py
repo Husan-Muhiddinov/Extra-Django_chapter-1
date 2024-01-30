523583405fbd5fa3c4eaf647023a958e0d09522c
Husan-Muhiddinov <124786003+Husan-Muhiddinov@users.noreply.github.com>
2024-01-30 18:37:57 +0500

    Extra Django AC

--- core/asgi.py
+++ core/asgi.py
@@ -1,9 +1,10 @@
-
-
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
@@ -11,5 +12,8 @@ application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
         # Just HTTP for now. (We can add other protocols later.)
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
+        ),
     }
 )
